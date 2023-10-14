ca439727f807aa4016b24868895f94b64bb2648e
KataM <gregofazz@hotmail.fr>
2023-10-14 14:17:18 -0400

    Add search area to research a city, rename the swiper

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import mySwiper from './components/mySwiper.vue'
+import PropertiesSwiper from './components/PropertiesSwiper.vue'
+import SearchBanner from './components/SearchBanner.vue'
 </script>
 
 <script>
@@ -9,10 +10,52 @@ export default {
       images: [
         // { src: '/src/assets/image1.jpg', alt: 'Image 1' },
         // { src: '/src/assets/image2.jpg', alt: 'Image 2' }
-      ]
+      ],
+      destinationData: {
+        title: 'Montreal Adventure',
+        description:
+          'Explore the vibrant city of Montreal, known for its rich culture and beautiful landscapes. Discover a variety of rental properties that cater to your comfort and style.',
+        rental_properties: [
+          {
+            name: 'Cozy Cabin Retreat',
+            rate: 4.8,
+            price_per_night: 120.0,
+            available_dates: ['2023-12-15', '2023-12-16', '2023-12-17']
+          },
+          {
+            name: 'Seaside Villa',
+            rate: 4.9,
+            price_per_night: 250.0,
+            available_dates: ['2023-12-20', '2023-12-21', '2023-12-22']
+          },
+          {
+            name: 'Mountain Chalet',
+            rate: 4.5,
+            price_per_night: 160.0,
+            available_dates: ['2023-12-18', '2023-12-19', '2023-12-20']
+          }
+        ]
+      }
     }
   },
-  mounted() {}
+  methods: {
+    fetchData() {
+      console.log('FETCH DATA CALLED')
+      fetch('http://localhost:3000/montreal', {
+        method: 'GET',
+        headers: {}
+      })
+        .then((response) => {
+          response.json().then((res) => console.log(res))
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
 }
 // import { RouterLink, RouterView } from 'vue-router'
 </script>
@@ -26,15 +69,15 @@ export default {
       </nav>
     </div> -->
 
+    <SearchBanner @search-clicked="fetchData" />
     <div class="container">
-      <h1>Discover the Magic of Our Destination</h1>
+      <h1>{{ destinationData.title }}</h1>
       <p>
-        Welcome to our breathtaking destination, where adventure awaits around every corner. Explore
-        the beauty of our city and experience a vacation like no other. From cozy cottages to
-        luxurious villas, we have a wide range of rental properties that will make your stay
-        unforgettable.
+        {{ destinationData.description }}
       </p>
-      <mySwiper />
+      <PropertiesSwiper />
+
+      <button @click="fetchData">Click Me!</button>
     </div>
   </header>
 </template>
---
+++ front-end/src/components/SearchBanner.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="search-banner">
+    <div class="search-card">
+      <div class="search-input">
+        <input type="text" placeholder="Search for a city" />
+        <button @click="search" class="search-button">Search</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    search() {
+      this.$emit('search-clicked')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-banner {
+  text-align: center;
+  padding: 80px 0;
+}
+
+.search-card {
+  display: inline-block;
+  background: #fff;
+  border-radius: 8px;
+  padding: 20px;
+  width: 300px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  transition:
+    box-shadow 0.3s,
+    transform 0.2s;
+  text-align: center;
+}
+
+.search-card:hover {
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+  transform: translateY(-2px);
+}
+
+.search-input {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+input {
+  flex: 1;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+}
+
+i {
+  margin-left: 10px;
+  color: #555;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+i:hover {
+  color: #333;
+}
+
+.search-button {
+  background: #ff5a5f;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  padding: 10px;
+  cursor: pointer;
+  margin-left: 10px;
+}
+
+.search-button:hover {
+  background: #e8494c;
+}
+</style>
