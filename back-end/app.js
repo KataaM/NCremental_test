9ac3eb7e841b591b8a26e3938fcedcb5d8bb3343
KataM <gregofazz@hotmail.fr>
2023-10-14 12:49:34 -0400

    Add the chatGPT API call to retrieve the destination information such as title and description, and return it to json format

--- back-end/app.js
+++ back-end/app.js
@@ -8,30 +8,47 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// The Json schema that will be used to retrieve chatGpt generation
 const jsonSchema = {
-  title: "string",
-  description: "string",
+  type: "object",
+  properties: {
+    title: {
+      type: "string",
+      description: "Descriptive title of the destination",
+    },
+    description: {
+      type: "string",
+      description: "Descriptive description the destination",
+    },
+  },
 };
 
-let APIcall = async (touristicDestination) => {
+// This function is used to call the chatGPT API and return in a json format the title and the description of our destination.
+let getDestinationContent = async (touristicDestination) => {
   try {
+    const contentMessage = `May you describe ${touristicDestination} with : -a title.\
+    The title will be used for SEO and should be generated to match the Search engine title restrictions.\
+     - a description. The description should be generated for the specific touristic destination and should advertise the available rental properties.!`;
+
     const chatCompletion = await openai.chat.completions.create({
       model: "gpt-4",
       messages: [
         {
           role: "user",
-          content:
-            'May you describe Montreal with : -a title.\
-       The title will be used for SEO and should be generated to match the Search engine title restrictions.\
-        - a description. The description should be generated for the specific touristic destination and should advertise the available rental properties.! \
-        Also, could you format it in a json way like { title : "", description : ""}',
+          content: contentMessage,
         },
       ],
+
+      functions: [{ name: "set_description", parameters: jsonSchema }],
+      function_call: { name: "set_description" },
     });
 
-    const output_text = chatCompletion.choices[0].message;
+    const generatedText =
+      chatCompletion.choices[0].message.function_call.arguments;
 
-    console.log(output_text);
+    const jsonObj = JSON.parse(generatedText);
+
+    return jsonObj;
   } catch (err) {
     if (err.response) {
       console.log(err.response.status);
@@ -46,11 +63,23 @@ app.listen(port, () => {
   console.log(`NCremental app listening on port ${port}`);
 });
 
+// Route that responds with "hello NCremental"
+app.get("/", (req, res) => {
+  res.send("Hello Ncremental!");
+});
+
 app.get("*", (req, res) => {
-  const requestedURL = req.url;
-  console.log(`Received a request for URL: ${requestedURL}`);
+  const requestedDestination = req.url.slice(1); // Remove the leading slash
+  console.log(`Received a request for URL: ${requestedDestination}`);
 
-  APIcall();
-
-  res.send(`${requestedURL}`);
+  // Call getDestinationContent and handle the response
+  getDestinationContent(requestedDestination)
+    .then((destinationContent) => {
+      // Send the content back as a JSON response
+      res.json(destinationContent);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 });
